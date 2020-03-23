2fe46a743def2a1443fd3dd4c84689c438088069
Marlena <maarl.deenis@gmail.com>
2020-03-23 17:02:05 +0100

    finished code from api's + geolocalisation

--- Lab4/classes/weather.js
+++ Lab4/classes/weather.js
@@ -6,17 +6,32 @@ constructor(){
 } // end constructor
 getLocation(){
     navigator.geolocation.getCurrentPosition(
-        this.myLocation.bind(this);
-        this.errorLocation.bind(this);
+        this.myLocation.bind(this),
+        this.errorLocation.bind(this)
     );
 }
 
 myLocation(result){
     this.lat = result.coords.latitude;
     this.lng = result.coords.longitude;
+    //console.log(this.lat);
+    this.getWeather();
 }
 
-getWeather();
+getWeather(){
+    // https://api.darksky.net/forecast/15684c4ffc14f32fcd28af8aa81bc818/37.8267,-122.4233
+    let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/15684c4ffc14f32fcd28af8aa81bc818/${this.lat},${this.lng}?units=si` 
+    fetch(url).then(response => {
+        //get json 
+         return response.json();
+    }).then(data =>{
+        console.log(data);
+        document.querySelector('#test').innerHTML = data.currently.summary;
+        document.querySelector('#test2').innerHTML = data.currently.temperature;
+    }).catch(err => {
+        console.log(err);
+    });
+};
 
 errorLocation(err){
     console.log(err);
@@ -24,3 +39,4 @@ errorLocation(err){
 } // end class Weather
 
 let weather = new Weather();
+

89f8702579b1778b621241409a117cf5c5f4f9ce
Marlena <maarl.deenis@gmail.com>
2020-03-23 21:54:03 +0100

    added export to data.js

---
+++ Lab4/data.js
@@ -0,0 +1,18 @@
+// connect pokemon with weather-icon
+let pokelist= {
+        "clear-day" : "pikatchu",
+        "clear-night" : "zubat",
+        "rain" : "wartortle",
+        "snow" : "lapras",
+        "sleet" : "horsea",
+        "wind" : "onix",
+        "fog" : "koffing",
+        "cloudy": "psydyck",
+        "partly-cloudy-day" : "bulbassaur",
+        "partly-cloudy-night" : "charmander",
+        "hail" : "dewgong",
+        "thunderstorm": "raichu",
+        "tornado" : "dragonite"
+};
+
+export {pokelist};

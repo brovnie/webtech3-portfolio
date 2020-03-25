7b8f9a31775a3380dfedd614998964e53459f3e8
Marlena <maarl.deenis@gmail.com>
2020-03-25 01:07:30 +0100

    gave value to weatherText

--- Lab4/classes/weather.js
+++ Lab4/classes/weather.js
@@ -3,7 +3,6 @@ class Weather {
     this.getLocation();
     this.lat;
     this.lng;
-    this.icon;
   } // end constructor
 
   getLocation() {
@@ -19,60 +18,77 @@ class Weather {
     //console.log(this.lat);
     this.getWeather();
   }
-
   getWeather() {
 
     let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/15684c4ffc14f32fcd28af8aa81bc818/${this.lat},${this.lng}?units=si`
+    console.log(this.lat);
+    console.log(this.lng);
     return fetch(url).then(response => {
       //get json 
       return response.json();
     }).then(data => {
-      document.querySelector('#test').innerHTML = data.currently.summary;
-      document.querySelector('#test2').innerHTML = data.currently.temperature + "deg";
+      let weatherText;
+
       //get pokemon
       let pokemonName = "";
       switch (data.currently.icon) {
         case 'clear-day':
           //pikacthu = 25
           pokemonName = "pikachu";
+          weatherText = "cear sky";
           break;
         case "clear-night":
           pokemonName = "zubat";
+          weatherText = "clear night ";
           break;
         case "rain":
           pokemonName = "wartortle";
+          weatherText = "raining";
           break;
         case "snow":
           pokemonName = "lapras";
+          weatherText = "snowing";
           break;
         case "sleet":
           pokemonName = "horsea";
+          weatherText = "sleeting";
           break;
         case "wind":
           pokemonName = "onix";
+          weatherText = "windy";
           break;
         case "fog":
           pokemonName = "koffing";
+          weatherText = "foggy";
           break;
         case "cloudy":
           pokemonName = "psydyck";
+          weatherText = "cloudy";
           break;
         case "partly-cloudy-day":
           pokemonName = "bulbassaur";
+          weatherText = "party cloudy";
           break;
         case "partly-cloudy-night":
           pokemonName = "charmander";
+          weatherText = "party cloudy night";
           break;
         case "hail":
           pokemonName = "dewgong";
+          weatherText = "hailing";
           break;
         case "thunderstorm":
           pokemonName = "raichu";
+          weatherText = "thunderstorm";
           break;
         case "tornado":
           pokemonName = "dragonite";
+          weatherText = "tornedo";
           break;
       }
+
+      document.querySelector('#test').innerHTML = "It's " + weatherText + " outside";
+      document.querySelector('#test2').innerHTML = data.currently.temperature + "deg";
       console.log(pokemonName);
       return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
       //this.icon = 'something that was returned in then';
@@ -85,10 +101,14 @@ class Weather {
       document.querySelector('#pokemon').setAttribute("src", data.sprites.front_default);
   }).catch(err => {
       console.log(err);
-      //this.icon = 'something that was returned in catch';
-      //return Promise.resolve(this.icon);
+      
     });
   };
+
+  getCityName( lat, long){
+    geocoder = new google.maps.Geocoder();
+
+  }
 
   errorLocation(err) {
     console.log(err);
